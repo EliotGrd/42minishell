43ff778313dbdbd079e5a0414399354ea8db5c2b
Benjamin Suger <bsuger@f6r5s16.clusters.42paris.fr>
2025-09-11 09:11:20 +0200

    fix the previous error mentioned regarding the heredoc + redirection => probably a previous change to try some stuff. It does not seems it break something else with what I remove (put it in comments)

--- src/parsing/parser.c
+++ src/parsing/parser.c
@@ -6,7 +6,7 @@
 /*   By: egiraud <egiraud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 17:16:42 by egiraud           #+#    #+#             */
-/*   Updated: 2025/09/10 14:20:42 by bsuger           ###   ########.fr       */
+/*   Updated: 2025/09/11 09:08:13 by bsuger           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ int	parse_redir(t_tokcursor *c, t_cmd *cur_cmd)
 		tail->next = create_node_redir(ft_strdup(c->current->next->lexeme),
 				c->current->type);
 	}
-	cur_next(c);//temp pour comprendre l'erreur
+	//cur_next(c);//temp pour comprendre l'erreur
 	cur_next(c);
 	return (1);
 }
